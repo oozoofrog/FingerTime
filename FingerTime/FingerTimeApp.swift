cd424b62221850b64547a8fb7343284f06305be0
oozoofrog <oozoofrog@gmail.com>
2026-05-03 01:23:22 +0900

    Initial Commit

---
+++ FingerTime/FingerTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FingerTimeApp.swift
+//  FingerTime
+//
+//  Created by oozoofrog on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct FingerTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FingerTimeTests/FingerTimeTests.swift
@@ -0,0 +1,19 @@
+//
+//  FingerTimeTests.swift
+//  FingerTimeTests
+//
+//  Created by oozoofrog on 5/3/26.
+//
+
+import Testing
+@testable import FingerTime
+
+struct FingerTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
